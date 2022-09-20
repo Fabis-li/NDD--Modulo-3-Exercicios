ecd4729a8a4f535d16b9f671c99c3abf7cccaec8
Fabiano <fabiano_slima@hotmail.com>
2022-09-20 15:54:52 -0300

    criacao controller banda

---
+++ Musica/Musica.Api/Controllers/BandaTocarController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Musica.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BandaTocarController : ControllerBase
+    {
+        // [HttpPost]
+        // String Descricao
+        // String NomeMusica
+        // int SequenciaMusica
+    }
+}
