e792d94c66ce476a8dd10ec18903206ffdd571b9
Fabiano <fabiano_slima@hotmail.com>
2022-09-25 21:12:41 -0300

    criacao banco

--- Musica/Musica.Infra/DataBase/BandaDbContext.cs
+++ Musica/Musica.Infra/DataBase/BandaDbContext.cs
@@ -1,16 +1,31 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Musica.Domain.Models;
+using Musica.Infra.DataBase.Mappings;
 
 namespace Musica.Infra.DataBase
 {
     public class BandaDbContext : DbContext
     {
-        public DbSet<BandaModel> Bandas{ get; set;}
+
+        
         private readonly IConfiguration _configuration;
         public BandaDbContext (IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+        public DbSet<BandaModel> Bandas{ get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            base.OnConfiguring(options);
+
+            options.UseSqlServer(_configuration.GetConnectionString("ConexaoBanco"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new BandaMap());
         }
     }
 }
---
+++ Musica/Musica.Infra/DataBase/Mappings/BandaMap.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Musica.Domain.Models;
+
+namespace Musica.Infra.DataBase.Mappings
+{
+    public class BandaMap : IEntityTypeConfiguration<BandaModel>
+    {
+        public void Configure(EntityTypeBuilder<BandaModel>builder)
+        {
+            builder.ToTable("MUSICAS");
+
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Id)
+                    .HasColumnName("ID")
+                    .HasColumnType("uniqueidentifier");
+            
+            builder.Property(m => m.Descricao)
+                    .HasColumnName("DESCRICAO")
+                    .HasColumnType("VARCHAR")
+                    .HasMaxLength(200);
+
+            builder.Property(m => m.NomeMusica)
+                    .HasColumnName("NOME_MUSICA")
+                    .HasColumnType("VARCHAR")
+                    .HasMaxLength(50);
+
+            builder.Property(m => m.SequenciaMusica)
+                    .HasColumnName("SEQUENCIA_MUSICA")
+                    .HasColumnType("int");
+        }
+    }
+}
