776f8c57b25ff9ed9791bf34aa0dd39cabd0593a
Fabiano <fabiano_slima@hotmail.com>
2022-10-16 14:23:16 -0300

    M3S07 ex6

--- Landing_page/registro-materia/src/app/app.component.ts
+++ Landing_page/registro-materia/src/app/app.component.ts
@@ -10,12 +10,18 @@ export class AppComponent {
   title = 'registro-materia';
 
   formRegsitroMateria = new FormGroup({
-    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    cargahoraria: new FormControl('',Validators.required),
-    escola: new FormControl('', Validators.required),
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern('^[A-Za-z]*$')]),
+    cargahoraria: new FormControl('',[Validators.required, Validators.min(30), Validators.max(100)]),
+    escola: new FormControl('', [Validators.required, Validators.pattern('^[A-Za-z]*$')]),
     professor: new FormControl('', [Validators.required]),
     materia: new FormControl('', Validators.required)
   })
 
+  resetForm(){
+    this.formRegsitroMateria.reset();
+  }
 
+  showForm(){
+    console.log(this.formRegsitroMateria)
+  }
 }
