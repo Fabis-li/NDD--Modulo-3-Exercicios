0b3ac75dc05f4322969ca0d39680735d43fa408a
Fabiano <fabiano_slima@hotmail.com>
2022-10-21 17:53:19 -0300

    M3S08 part1

---
+++ Cypress/CalcSimples/cypress/e2e/calcSimples/Exercicios.cy.js
@@ -0,0 +1,36 @@
+context('testesExerc', () => {
+
+    before(() => {
+        cy.visit('https://localhost:44360/CalcSimples.aspx')
+    });
+
+    afterEach(() => {
+        cy.get('input').contains('Calcular').click()
+        cy.get('input').contains('Limpar').click()
+    });
+    it('Calc10+123', () => {       
+        
+        cy.get('#txtN1').type(10)
+        cy.get('#txtN2').type(123)        
+        
+    });
+
+     it('Calc35-25', () => {
+        cy.get('#txtN1').type(35)
+        cy.get('#rbFuncao_1').click()
+        cy.get('#txtN2').type(25) 
+     });
+
+     it('Calc5*55', () => {
+        cy.get('#txtN1').type(5)
+        cy.get('#rbFuncao_2').click()
+        cy.get('#txtN2').type(55) 
+     });
+
+     it('Calc20*2', () => {
+        cy.get('#txtN1').type(20)
+        cy.get('#rbFuncao_3').click()
+        cy.get('#txtN2').type(2) 
+     });
+    
+});
