023cbbf014de02b69e176c6e7bae18d9cd64258f
Fabiano <fabiano_slima@hotmail.com>
2022-10-23 16:31:05 -0300

    M3S08 ex4

--- Cypress/ControleEntradasSaidas/cypress/e2e/entradaSaidas.cy.js
+++ Cypress/ControleEntradasSaidas/cypress/e2e/entradaSaidas.cy.js
@@ -13,8 +13,83 @@ context('EntradaSaidas ex1', () => {
         cy.get('#txtN4').type('Refrigerante')
         cy.get('#txtV4').type('6')
 
-        cy.get('#btnNovaEntradaSaida').click()
+        cy.get('#btnNovaEntradaSaida').click()       
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Boneca')
+                    }
+                    if(index == 2){
+                        expect(nome).to.be.equal('Carrinho')
+                    }
+                    if(index == 3){
+                        expect(nome).to.be.equal('Doces')
+                    }
+                    if(index == 4){
+                        expect(nome).to.be.equal('Refrigerante')
+                    }
+                    
+                })
+            }
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('4')
+                    }
+                    if(index == 2){
+                        expect(vlr).to.be.equal('1')
+                    }
+                    if(index == 3){
+                        expect(vlr).to.be.equal('5')
+                    } 
+                    if(index == 4){
+                        expect(vlr).to.be.equal('6')
+                    }                          
+                })              
+              
+            }
+        })
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 16')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN2').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV2').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN3').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV3').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN4').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV4').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        
     });
 
     it('Entrada2', () => {
@@ -26,7 +101,69 @@ context('EntradaSaidas ex1', () => {
         cy.get('#txtV3').type('8')
         
         cy.get('#btnNovaEntradaSaida').click()
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Boneca')
+                    }
+                    if(index == 2){
+                        expect(nome).to.be.equal('Bolo')
+                    }
+                    if(index == 3){
+                        expect(nome).to.be.equal('Doces')
+                    }
+                                       
+                })
+            }
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('78')
+                    }
+                    if(index == 2){
+                        expect(vlr).to.be.equal('3')
+                    }
+                    if(index == 3){
+                        expect(vlr).to.be.equal('8')
+                    }                                           
+                })              
+              
+            }
+        })        
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 89')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN2').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV2').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN3').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV3').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
     });
 
     it('Entrada3', () => {
@@ -42,6 +179,94 @@ context('EntradaSaidas ex1', () => {
         cy.get('#txtV5').type('2')
 
         cy.get('#btnNovaEntradaSaida').click()
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Café')
+                    }
+                    if(index == 2){
+                        expect(nome).to.be.equal('Coxinha')
+                    }
+                    if(index == 3){
+                        expect(nome).to.be.equal('Pavê')
+                    }
+                    if(index == 4){
+                        expect(nome).to.be.equal('Bolo Seco')
+                    }
+                    if(index == 5){
+                        expect(nome).to.be.equal('Gelatina')
+                    }
+                    
+                })
+            }
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('2')
+                    }
+                    if(index == 2){
+                        expect(vlr).to.be.equal('3')
+                    }
+                    if(index == 3){
+                        expect(vlr).to.be.equal('1')
+                    } 
+                    if(index == 4){
+                        expect(vlr).to.be.equal('1')
+                    }
+                    if(index == 5){
+                        expect(vlr).to.be.equal('2')
+                    }                           
+                })              
+              
+            }
+        })
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 9')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN2').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV2').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN3').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV3').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN4').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV4').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN5').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV5').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+       
+
     });
 });
--- Cypress/ControleEntradasSaidas/cypress/e2e/entradaSaidas2.cy.js
+++ Cypress/ControleEntradasSaidas/cypress/e2e/entradaSaidas2.cy.js
@@ -10,14 +10,103 @@ context('EntradaSaidaEx2', () => {
         cy.get('#txtV2').type('530')
 
         cy.get('#btnNovaEntradaSaida').click()
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Caneta')
+                    }
+                    if(index == 2){
+                        expect(nome).to.be.equal('Folha A4')
+                    }                   
+                })              
+              
+            }
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('234')
+                    }
+                    if(index == 2){
+                        expect(vlr).to.be.equal('530')
+                    }                   
+                })              
+              
+            }
+        })
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 764')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN2').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV2').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        
     });
     it('Entrada2', () => {
         cy.get('#txtN1').type('Lápis')
         cy.get('#txtV1').type('4320')        
 
         cy.get('#btnNovaEntradaSaida').click()
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Lápis')
+                    }                  
+                    
+                })
+            }
+        })
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('4320')
+                    }
+                                     
+                })              
+              
+            }
+        })
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 4320')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
     });
     it('Entrada3', () => {
         cy.get('#txtN1').type('Bolsa')
@@ -28,7 +117,70 @@ context('EntradaSaidaEx2', () => {
         cy.get('#txtV3').type('1200')        
 
         cy.get('#btnNovaEntradaSaida').click()
+        
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    if(index == 1){
+                        expect(nome).to.be.equal('Bolsa')
+                    }
+                    if(index == 2){
+                        expect(nome).to.be.equal('Caderno')
+                    }
+                    if(index == 3){
+                        expect(nome).to.be.equal('Livros')
+                    }               
+                    
+                })
+            }
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index, $list) => {
+            if(index > 0){
+                cy.get($el).find('#pValor')
+                .invoke('text').then(vlr => {
+                    if(index == 1){
+                        expect(vlr).to.be.equal('135')
+                    }
+                    if(index == 2){
+                        expect(vlr).to.be.equal('1200')
+                    }
+                    if(index == 3){
+                        expect(vlr).to.be.equal('1200')
+                    }                      
+                })              
+              
+            }
+        })
+        
+
+        cy.get('#lbVTotal').invoke('text').then(val => {
+            expect(val).to.be.equal('Valor Total: 2535')
+        })
+
         cy.get('#btnLimpar').click()
+
+        cy.get('#txtN1').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV1').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN2').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV2').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtN3').invoke('text').then(texto => {
+            expect([]).to.be.empty
+        })
+        cy.get('#txtV3').invoke('text').then(vlr => {
+            expect([]).to.be.empty
+        })
     });
 
 });
